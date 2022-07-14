e9517512743449a08a6807bb6c0333c8a561180b
Vlado Jovanovic <vladojovanovic20@gmail.com>
2022-07-14 14:39:38 +0200

    Added images

--- VSCode Implementation/binaryTomo.py
+++ VSCode Implementation/binaryTomo.py
@@ -193,7 +193,7 @@ def linear_coolingSA(sinogram, angles):
 start_time = time.time()
 fig = plt.figure(figsize=(10, 7))
 
-image = binarizeWithSize("barn-owl.png", 64)
+image = binarizeWithSize("horseshoe.jpeg", 64)
 
 np.random.seed(107)
 # number of projections
@@ -206,7 +206,7 @@ angles = np.linspace(0, 180, projectionNo)
 sinogram = radon(image, theta=angles)
 
 # noise
-noise = np.random.normal(0, 3, (sinogram.shape[0], sinogram.shape[1]))
+noise = np.random.normal(0, 1, (sinogram.shape[0], sinogram.shape[1]))
 sinogram = sinogram + noise
 
 reconstruction = linear_coolingSA(sinogram, angles)
